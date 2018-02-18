bf77377609cab58dda5bfffa696c8f9609e2e965
Konstantin Labun <konstantin.labun@gmail.com>
2018-02-18 09:51:23 +0100

    sync to plugin update

--- init.sql
+++ init.sql
@@ -7,4 +7,15 @@ CREATE TABLE person (
   created         DATETIME,
   last_modified   TIMESTAMP,
   profile_picture LONGBLOB
-)
+);
+
+CREATE TABLE person2 (
+  id              BIGINT PRIMARY KEY,
+  first_name      VARCHAR(256),
+  last_name       VARCHAR(256),
+  age             INTEGER,
+  birth_date      DATE,
+  created         DATETIME,
+  last_modified   TIMESTAMP,
+  profile_picture LONGBLOB
+);
